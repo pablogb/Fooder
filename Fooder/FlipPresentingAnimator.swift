156eaac82a907e3eca4eaac89307080ec1eadbde
Pablo Gomez <pablogb@gmail.com>
2016-07-14 00:56:57 -0500

    Change PresentingAnimator to use basic animations to showcase both basic and spring animations

--- Fooder/FlipPresentingAnimator.swift
+++ Fooder/FlipPresentingAnimator.swift
@@ -54,47 +54,38 @@ class FlipPresentingAnimator: NSObject, UIViewControllerAnimatedTransitioning {
                 
         let animationDuration = transitionDuration(using: transitionContext)
         
-        toSnapshotView.layer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2.0, 0.0, 1.0, 0.0)
+        let firstFlipAnimation = POPBasicAnimation(propertyNamed: kPOPLayerRotationY)!
+        firstFlipAnimation.duration = animationDuration / 2.0
+        firstFlipAnimation.toValue = CGFloat.pi / 2.0
+        firstFlipAnimation.completionBlock = {(animation, finished) in
+            fromSnapshotView.isHidden = true // Keep this snapshot around for the return animation
+            toSnapshotView.isHidden = false
+        }
         
-        let firstSpringVelocity:CGFloat = 10.0
-        let firstSpringBounciness:CGFloat = 0.0
-        let secondSpringVelocity:CGFloat = 5.0
-        let secondSpringBounciness:CGFloat = 10.0
+        let firstScaleAnimation = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)!
+        firstScaleAnimation.duration = animationDuration / 2.0
+        firstScaleAnimation.toValue = NSValue(cgSize: CGSize(width: 1.25, height: 1.25))
         
-        let secondFlipAnimation = POPSpringAnimation(propertyNamed: kPOPLayerRotationY)!
-        secondFlipAnimation.velocity = secondSpringVelocity
-        secondFlipAnimation.springBounciness = secondSpringBounciness
-        secondFlipAnimation.toValue = 0.0
+        fromSnapshotView.layer.pop_add(firstFlipAnimation, forKey: "firstFlip")
+        fromSnapshotView.layer.pop_add(firstScaleAnimation, forKey: "firstScale")
+        
+        let secondFlipAnimation = POPBasicAnimation(propertyNamed: kPOPLayerRotationY)!
+        secondFlipAnimation.duration = animationDuration / 2.0
+        secondFlipAnimation.beginTime = CACurrentMediaTime() + animationDuration / 2.0
+        secondFlipAnimation.fromValue = -CGFloat.pi / 2.0
         secondFlipAnimation.completionBlock = {(animation, finished) in
             toView.isHidden = false
             toSnapshotView.removeFromSuperview()
             transitionContext.completeTransition(true)
         }
         
-        let secondScaleAnimation = POPSpringAnimation(propertyNamed: kPOPLayerScaleXY)!
-        secondScaleAnimation.velocity = NSValue(cgPoint: CGPoint(x: 0.0, y: secondSpringVelocity))
-        secondScaleAnimation.springBounciness = secondSpringBounciness
+        let secondScaleAnimation = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)!
+        secondScaleAnimation.duration = animationDuration / 2.0
+        secondScaleAnimation.beginTime = CACurrentMediaTime() + animationDuration / 2.0
         secondScaleAnimation.fromValue = NSValue(cgSize: CGSize(width: 1.25, height: 1.25))
         
-        let firstFlipAnimation = POPSpringAnimation(propertyNamed: kPOPLayerRotationY)!
-        firstFlipAnimation.velocity = firstSpringVelocity
-        firstFlipAnimation.springBounciness = firstSpringBounciness
-        firstFlipAnimation.toValue = CGFloat.pi / 2.0
-        firstFlipAnimation.completionBlock = {(animation, finished) in
-            fromSnapshotView.isHidden = true // Keep this snapshot around for the return animation
-            toSnapshotView.isHidden = false
-            
-            toSnapshotView.layer.pop_add(secondFlipAnimation, forKey: "secondFlip")
-            toSnapshotView.layer.pop_add(secondScaleAnimation, forKey: "secondScale")
-        }
-        
-        let firstScaleAnimation = POPSpringAnimation(propertyNamed: kPOPLayerScaleXY)!
-        firstScaleAnimation.velocity = NSValue(cgPoint: CGPoint(x: 0.0, y: firstSpringVelocity))
-        firstScaleAnimation.springBounciness = firstSpringBounciness
-        firstScaleAnimation.toValue = NSValue(cgSize: CGSize(width: 1.25, height: 1.25))
-        
-        fromSnapshotView.layer.pop_add(firstFlipAnimation, forKey: "firstFlip")
-        fromSnapshotView.layer.pop_add(firstScaleAnimation, forKey: "firstScale")
+        toSnapshotView.layer.pop_add(secondFlipAnimation, forKey: "secondFlip")
+        toSnapshotView.layer.pop_add(secondScaleAnimation, forKey: "secondScale")
         
         // Dimming View animation
         let opacityAnimation = POPBasicAnimation(propertyNamed: kPOPLayerOpacity)!
